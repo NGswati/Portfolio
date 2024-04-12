89eb9b098e4945aae7916c44d84412d20a1934f5
NGswati <swatihass@gmail.com>
2024-04-12 15:23:54 +0530

    Added main.js file

---
+++ src/Main.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Intro from './components/intro';
+import App from './components/app';
+import School from './components/school';
+// import Experience from './pages/Experience';
+// import Research from './pages/Research';
+// import Contact from './pages/Contact';
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+
+function Main() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Intro />} />
+                <Route path="/app" element={<App />} />
+                <Route path="/school" element={<School />} />
+                {/* <Route path="/experience" element={<Experience />} />
+                <Route path="/research" element={<Research />} />
+                <Route path="/contact" element={<Contact />} /> */}
+            </Routes>
+        </Router>
+    );
+}
+
+export default Main;
